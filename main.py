96c185a09a7f8ea347b0e2e2cb4ce71edf3f36e9
BoloniniD <denis@bolonin.ru>
2021-07-13 12:04:26 +0300

    Applied autoformatting, added /enable and /disable

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
+import os.path
 import pickle
 import sys
-import os.path
+
 import googletrans
 import telebot
 
 
 class TranslatorBot:
     def __init__(self):
-        self.all_langs = str(googletrans.LANGUAGES).replace(', ', '\n').replace("'", "")[1:-1]
+        self.all_langs = (
+            str(googletrans.LANGUAGES).replace(", ", "\n").replace("'", "")[1:-1]
+        )
+        self.enabled = True
         self.configuring = False
         self.deleting = False
-        cfg = open('token.txt', 'r')
+        cfg = open("token.txt", "r")
         try:
             # read the token from cfg file
             token = cfg.readline()
             self.bot = telebot.TeleBot(token, parse_mode=None)
-
-            # read the translator parameters from cfg file
-            # "src:dest"
-            self.translator = googletrans.Translator()
-            self.groups = {}
         except:
             print("Cannot open configs")
             cfg.close()
@@ -27,34 +26,49 @@ class TranslatorBot:
         finally:
             cfg.close()
 
-        if (not os.path.exists("chat_cfg.cfg")):
-            file = open("chat_cfg.cfg", "wb") 
+        # read the translator parameters from cfg file
+        # "src:dest"
+        self.translator = googletrans.Translator()
+        self.groups = {}
+
+        if not os.path.exists("chat_cfg.cfg"):
+            file = open("chat_cfg.cfg", "wb")
             file.close()
 
         self.loadConfig()
 
-        @self.bot.message_handler(commands=['start'])
+        @self.bot.message_handler(commands=["start"])
         def startBot(message):
             print("Received help message")
-            self.bot.reply_to(message, "This bot will translate all user messages in this chat.\nYou can configure which languages need to be translated and what the target language should be with /add_config command.\nUse /help to see other commands.")
+            self.bot.reply_to(
+                message,
+                "This bot will translate all user messages in this chat.\nYou can configure which languages need to be translated and what the target language should be with /add_config command.\nUse /help to see other commands.",
+            )
 
-        @self.bot.message_handler(commands=['help'])
+        @self.bot.message_handler(commands=["help"])
         def startBot(message):
             print("Received help message")
-            self.bot.reply_to(message, '''This bot has the following commands:
+            self.bot.reply_to(
+                message,
+                """This bot has the following commands:
 /start - starting message
 /help - this help page
 /add_config - add a langauge pair src:dest, where src - is source language and dest is target language. For src and dest you should use language codes, which can be aquired with /all_langs command.
 /all_langs - bot will send you a PM with all supported languages
-/rm_config - this command will list all present configuration pairs and will wait a src:dest pair, which will be deleted from config''')
-           
-        @self.bot.message_handler(commands=['add_config'])
+/rm_config - this command will list all present configuration pairs and will wait a src:dest pair, which will be deleted from config
+/show_config - list all current configuration pairs""",
+            )
+
+        @self.bot.message_handler(commands=["add_config"])
         def addConfigBot(message):
             print("Received add_config message")
             self.configuring = True
-            self.bot.reply_to(message, "Choose a source language and destination language.\nNow you can write messages with 'src:dest' to add new translation configs, where src is source language and dest is target language.\nFor example, en:fr.")
+            self.bot.reply_to(
+                message,
+                "Choose a source language and destination language.\nNow you can write messages with 'src:dest' to add new translation configs, where src is source language and dest is target language.\nFor example, en:fr.",
+            )
 
-        @self.bot.message_handler(commands=['all_langs'])
+        @self.bot.message_handler(commands=["all_langs"])
         def showLangsBot(message):
             print("Received all_langs message")
             self.bot.reply_to(message, "I will send you all supported languages")
@@ -62,32 +76,78 @@ class TranslatorBot:
                 self.bot.send_message(message.from_user.id, self.all_langs)
             except:
                 print("Failed to send the message to used", message.from_user.id)
-                self.bot.reply_to(message, "Failed to send you a PM. Please, check if you have blocked the bot.")
+                self.bot.reply_to(
+                    message,
+                    "Failed to send you a PM. Please, check if you have blocked the bot.",
+                )
 
-        @self.bot.message_handler(commands=['show_config'])
+        @self.bot.message_handler(commands=["show_config"])
         def showConfigBot(message):
             print("Received show_config message")
             if not message.chat.id in self.groups:
-                self.bot.reply_to(message, "No configuration pairs in this chat. Nothing to delete.")
+                self.bot.reply_to(
+                    message, "No configuration pairs in this chat. Nothing to delete."
+                )
                 return
             if not bool(self.groups[message.chat.id]):
-                self.bot.reply_to(message, "No configuration pairs in this chat. Nothing to delete.")
+                self.bot.reply_to(
+                    message, "No configuration pairs in this chat. Nothing to delete."
+                )
                 return
-            s = ', '.join([':'.join((k, str(self.groups[message.chat.id][k]))) for k in sorted(self.groups[message.chat.id], key=self.groups[message.chat.id]. get, reverse=True)])
+            s = ", ".join(
+                [
+                    ":".join((k, str(self.groups[message.chat.id][k])))
+                    for k in sorted(
+                        self.groups[message.chat.id],
+                        key=self.groups[message.chat.id].get,
+                        reverse=True,
+                    )
+                ]
+            )
             self.bot.reply_to(message, "All configuration pairs: " + s + ".")
 
-        @self.bot.message_handler(commands=['rm_config'])
+        @self.bot.message_handler(commands=["enable"])
+        def enable(message):
+            print("Received enable command")
+            self.enabled = True
+            self.bot.reply_to(message, "Bot enabled")
+
+        @self.bot.message_handler(commands=["disable"])
+        def disable(message):
+            print("Received disable command")
+            self.enabled = False
+            self.bot.reply_to(message, "Bot disabled")
+
+        @self.bot.message_handler(commands=["rm_config"])
         def delConfigBot(message):
             print("Received rm_config message")
             if not message.chat.id in self.groups:
-                self.bot.reply_to(message, "No configuration pairs in this chat. Nothing to delete.")
+                self.bot.reply_to(
+                    message, "No configuration pairs in this chat. Nothing to delete."
+                )
                 return
             if not bool(self.groups[message.chat.id]):
-                self.bot.reply_to(message, "No configuration pairs in this chat. Nothing to delete.")
+                self.bot.reply_to(
+                    message, "No configuration pairs in this chat. Nothing to delete."
+                )
                 return
             self.deleting = True
-            s = ', '.join([':'.join((k, str(self.groups[message.chat.id][k]))) for k in sorted(self.groups[message.chat.id], key=self.groups[message.chat.id]. get, reverse=True)])
-            self.bot.reply_to(message, "All configuration pairs: " + s + ".\nChoose one and write a message with it. This pair will be removed from config.")
+            s = ", ".join(
+                [
+                    ":".join((k, str(self.groups[message.chat.id][k])))
+                    for k in sorted(
+                        self.groups[message.chat.id],
+                        key=self.groups[message.chat.id].get,
+                        reverse=True,
+                    )
+                ]
+            )
+            self.bot.reply_to(
+                message,
+                "All configuration pairs: "
+                + s
+                + ".\nChoose one and write a message with it. This pair will be removed from config.",
+            )
 
         @self.bot.message_handler(func=lambda message: True)
         def getMessages(message):
@@ -100,18 +160,28 @@ class TranslatorBot:
                 print("Received text message while deleting cfg pair")
                 self.delConfigPair(message)
                 self.deleting = False
-            else:
+            elif self.enabled:
                 print("Received text message")
                 try:
-                    result = self.translator.translate(message.text, dest=self.groups[message.chat.id][self.translator.detect(message.text).lang])
+                    result = self.translator.translate(
+                        message.text,
+                        dest=self.groups[message.chat.id][
+                            self.translator.detect(message.text).lang
+                        ],
+                    )
                     self.bot.reply_to(message, result.text)
                     print("Translated the message successfully")
                 except:
-                    print("Skipping unknown language", self.translator.detect(message.text).lang)
+                    print(
+                        "Skipping unknown language",
+                        self.translator.detect(message.text).lang,
+                    )
+            else:
+                print("Disabled")
 
     def addConfigPair(self, message):
         try:
-            langs = message.text.split(':')
+            langs = message.text.split(":")
             langs[0] = langs[0].lower()
             langs[1] = langs[1].lower()
             if not (langs[0] in googletrans.LANGUAGES):
@@ -145,17 +215,20 @@ class TranslatorBot:
 
     def delConfigPair(self, message):
         try:
-            langs = message.text.split(':')
+            langs = message.text.split(":")
+            langs[0] = langs[0].lower()
+            langs[1] = langs[1].lower()
             self.groups[message.chat.id].pop(langs[0])
             self.bot.reply_to(message, "Removed a source/target pair.")
             print("Removed src/dest pair")
         except:
             self.bot.reply_to(message, "String format is incorrect, please try again")
             print("Failed to remove src/dest pair")
-            
+
     def launchBot(self):
         print("Launching bot...")
         self.bot.polling()
+
 
 tbot = TranslatorBot()
 tbot.launchBot()
